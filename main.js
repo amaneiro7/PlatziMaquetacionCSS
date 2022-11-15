0719a1fecb019a9a677caaf385a91388bc0931c4
Andres Maneiro <amaneiro7@gmail.com>
2022-11-15 17:15:39 -0400

    Se agrego un avance de los slides

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 const btn1 = document.querySelector('#btn1')
+const btn2 = document.querySelector('#btn2')
+const btn3 = document.querySelector('#btn3')
 const modal = document.querySelector('.modal')
+const close = document.querySelector('.modal-content-close')
+
+btn1.addEventListener('click', () => {
+    modal.classList.remove('hidden')
+    modal.classList.add('visible')    
+})
+btn2.addEventListener('click', () => {
+    modal.classList.remove('hidden')
+    modal.classList.add('visible')    
+})
+btn3.addEventListener('click', () => {
+    modal.classList.remove('hidden')
+    modal.classList.add('visible')    
+})
+
+close.addEventListener('click', () => {
+    modal.classList.add('hidden')
+    modal.classList.remove('visible')    
+})
